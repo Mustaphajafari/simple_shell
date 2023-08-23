4281604f2939081c633c4b783bb7489751a4edc2
Mustapha Jafari <jaafarimustapha28@gmail.com>
2023-08-23 05:41:49 -0700

    h

--- _find_in_path.c
+++ _find_in_path.c
@@ -96,4 +96,26 @@ char **tokenize_path(data_of_program *data)
 	free(PATH);
 	return (tokens);
 }
+/**
+ * check_file - checks if exists a file, if it is not a dairectory and
+ * if it has excecution permisions for permisions.
+ * @full_path: pointer to the full file name
+ * Return: 0 on success, or error code if it exists.
+ */
+int check_file(char *full_path)
+{
+	struct stat sb;
 
+	if (stat(full_path, &sb) != -1)
+	{
+		if (S_ISDIR(sb.st_mode) ||  access(full_path, X_OK))
+		{
+			errno = 126;
+			return (126);
+		}
+		return (0);
+	}
+	/*if not exist the file*/
+	errno = 127;
+	return (127);
+}
